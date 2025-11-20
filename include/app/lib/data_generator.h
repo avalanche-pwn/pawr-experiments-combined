11786f175707db8fea4a482acf66d0f0b61daff9
Antoni Duda <antoni.duda@programmer.net>
2025-11-20 20:59:30 +0100

    feat(data_generator): Introducing data generator

---
+++ include/app/lib/data_generator.h
@@ -0,0 +1,29 @@
+#ifndef APP_LIB_DATA_GENERATOR_H
+#define APP_LIB_DATA_GENERATOR_H
+
+#include <stdint.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/atomic.h>
+#include <zephyr/net_buf.h>
+#include <zephyr/random/random.h>
+
+/**
+ * \brief Struct which is used to configure data generator.
+ * After each \ref
+ * interval ms a net_buf_simple with \ref len data is generated.  The caller is
+ * notified about the generator via \ref generated function.
+ */
+typedef struct {
+    struct net_buf_simple *data;
+    int interval;
+    void (*generated)();
+} data_generator_config_t;
+
+/**
+ * Initializes data generator
+ */
+void data_generator_init(data_generator_config_t *config); 
+void data_generator_stop();
+
+#endif // APP_LIB_DATA_GENERATOR_H
---
+++ lib/data_generator/data_generator.c
@@ -0,0 +1,28 @@
+#include <app/lib/data_generator.h>
+
+static void generate_data(struct k_timer *timer);
+static void cleanup_fn(struct k_timer *timer);
+
+K_TIMER_DEFINE(data_gen_timer, &generate_data, NULL);
+static data_generator_config_t *config_p;
+
+
+void data_generator_init(data_generator_config_t *config) {
+    config_p = config;
+    k_timer_start(&data_gen_timer, K_SECONDS(config->interval), K_SECONDS(config->interval));
+}
+
+void data_generator_stop() {
+    k_timer_stop(&data_gen_timer);
+}
+
+static void generate_data(struct k_timer *timer){
+    uint8_t data[config_p->data->len];
+    sys_rand_get(&data, config_p->data->len);
+    net_buf_simple_reset(config_p->data);
+    net_buf_simple_push_mem(config_p->data, &data, config_p->data->len);
+
+    if (config_p->generated)
+        config_p->generated();
+};
+
