101db820b3d7e1eefb300ab5cf7cabb528ce54ca
Antoni Duda <antoni.duda@programmer.net>
2025-11-22 21:55:59 +0100

    feat(crypto): Introducing secure counter and fix bugs

--- include/app/lib/crypto.h
+++ include/app/lib/crypto.h
@@ -2,6 +2,8 @@
 #define APP_LIB_CRYPTO_H
 
 #include <psa/crypto.h>
+#include <psa/internal_trusted_storage.h>
+
 #include <stdint.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/net_buf.h>
@@ -10,8 +12,10 @@
 #define HMAC_SHA_256 PSA_ALG_HMAC(PSA_ALG_SHA_256)
 #define HASH_LEN PSA_HASH_LENGTH(HMAC_SHA_256)
 #define KEY_BITS 256
+#define KEY_LEN (KEY_BITS / 8)
 #define KEY_FLAGS PSA_KEY_USAGE_SIGN_MESSAGE
 #define MAC_LEN PSA_MAC_LENGTH(PSA_KEY_TYPE_HMAC, KEY_BITS, HMAC_SHA_256)
+#define COUNTER_DATA_FLAGS PSA_STORAGE_FLAG_NO_CONFIDENTIALITY
 
 #define CRYPTO_KEY_DEFINE_STATIC(name)                                         \
     NET_BUF_SIMPLE_DEFINE_STATIC(name, KEY_BITS / 8)
@@ -22,7 +26,7 @@
  * \brief Initialize PSA with psa_crypto_init
  * \return 0 on success negative error on fail
  */
-int crypto_init();
+psa_status_t crypto_init();
 /**
  * \brief Saves persistent key.
  * \param persistent_id Id with wich the key is saved
@@ -30,7 +34,7 @@ int crypto_init();
  *
  * \return 0 on success, negative error code otherwise
  */
-int crypto_save_persistent_key(psa_key_id_t persistent_id,
+psa_status_t crypto_save_persistent_key(psa_key_id_t persistent_id,
                                struct net_buf_simple key);
 /**
  * \brief Compute HMAC value and store it in mac_out
@@ -40,7 +44,15 @@ int crypto_save_persistent_key(psa_key_id_t persistent_id,
  *
  * \return 0 on success, error code otherwise
  */
-int crypto_compute_mac(psa_key_id_t key_id, struct net_buf_simple input,
-                       struct net_buf_simple mac_out);
+psa_status_t crypto_compute_mac(psa_key_id_t key_id, struct net_buf_simple input,
+                       size_t hashable_len, struct net_buf_simple mac_out);
+
+typedef struct {
+    uint64_t value;
+    psa_storage_uid_t storage_uid;
+} crypto_counter_t;
+
+psa_status_t crypto_secure_counter_init(crypto_counter_t *ctx);
+psa_status_t crypto_secure_counter_commit(crypto_counter_t *ctx);
 
 #endif // APP_LIB_CRYPTO_H
--- lib/crypto/crypto.c
+++ lib/crypto/crypto.c
@@ -1,15 +1,12 @@
 #include <app/lib/crypto.h>
 
-int crypto_init() {
-    if (psa_crypto_init() != PSA_SUCCESS)
-        return -1;
-    return 0;
+inline psa_status_t crypto_init() {
+    return psa_crypto_init();
 }
 
-int crypto_save_persistent_key(psa_key_id_t persistent_id,
+psa_status_t crypto_save_persistent_key(psa_key_id_t persistent_id,
                                struct net_buf_simple key) {
     psa_key_id_t ret_id;
-    psa_status_t err;
     psa_key_attributes_t attributes;
 
     psa_set_key_id(&attributes, persistent_id);
@@ -19,24 +16,35 @@ int crypto_save_persistent_key(psa_key_id_t persistent_id,
     psa_set_key_usage_flags(&attributes, KEY_FLAGS);
     psa_set_key_algorithm(&attributes, HMAC_SHA_256);
 
-    err = psa_import_key(&attributes, key.data, key.len, &ret_id);
-    if (err != PSA_SUCCESS)
-        return -1;
-    return 0;
+    return psa_import_key(&attributes, key.data, key.len, &ret_id);
 }
 
 int crypto_compute_mac(psa_key_id_t key_id, struct net_buf_simple input,
-                       struct net_buf_simple mac_out) {
+                       size_t hashable_len, struct net_buf_simple mac_out) {
     size_t out_len;
+    uint8_t *mac_data = net_buf_simple_add(&mac_out, MAC_LEN);
+
+    return psa_mac_compute(key_id, HMAC_SHA_256, input.data, hashable_len,
+                          mac_data, MAC_LEN, &out_len);
+}
+
+psa_status_t crypto_secure_counter_init(crypto_counter_t *ctx) {
     psa_status_t err;
-
-    err = psa_mac_compute(key_id, HMAC_SHA_256, input.data, input.len, mac_out.data,
-                    mac_out.size, &out_len);
-
-    mac_out.len = out_len;
-
-    if (err != PSA_SUCCESS) {
-        return err;
+    size_t read;
+    uint8_t tmp[sizeof(ctx->value)];
+    err = psa_its_get(ctx->storage_uid, 0, sizeof(ctx->value), &ctx->value,
+                      &read);
+    if (err == PSA_ERROR_DOES_NOT_EXIST) {
+        psa_generate_random(tmp, sizeof(ctx->value));
+        for (size_t i = 0; i < sizeof(ctx->value); i++) {
+            ctx->value |= tmp[i] << i * 8;
+        }
+        return crypto_secure_counter_commit(ctx);
     }
-    return 0;
+    return err;
+}
+
+inline psa_status_t crypto_secure_counter_commit(crypto_counter_t *ctx) {
+    return psa_its_set(ctx->storage_uid, sizeof(ctx->value), &ctx->value,
+                       COUNTER_DATA_FLAGS);
 }
