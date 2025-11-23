e84dc4bcff27fcf065eea22cfbf33a16954fb5fe
Antoni Duda <antoni.duda@programmer.net>
2025-11-23 17:01:34 +0100

    feat(crypto_flasher): Adjust to changes in crypto

--- crypto_flasher/src/main.c
+++ crypto_flasher/src/main.c
@@ -2,16 +2,15 @@
 #include <zephyr/net_buf.h>
 
 #include <app/lib/crypto.h>
+#include <app/lib/common.h>
 
 uint8_t keys[3][KEY_LEN] = {
     "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
     "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
     "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC",
 };
-const psa_key_type_t advertiser_key_id =
-    PSA_KEY_ID_USER_MIN + CONFIG_ADVERTISER_KEY_ID_OFFSET;
-const psa_key_type_t min_scanner_key_id =
-    advertiser_key_id + CONFIG_SCANNER_MIN_KEY_ID;
+
+crypto_counter_t counter = {.storage_uid=COUNTER_ID};
 
 BUILD_ASSERT(CONFIG_FLASHED_DEVICE != -1,
              "You need to set id of device to flash");
@@ -20,21 +19,36 @@ psa_key_id_t recreate_key(psa_key_id_t id, struct net_buf_simple key) {
     psa_status_t err = psa_destroy_key(id);
     if (err != PSA_SUCCESS && err != PSA_ERROR_INVALID_HANDLE)
         return err;
-    return crypto_save_persistent_key(id, key);
+    return crypto_save_persistent_key(id, &key);
 }
 
 int main() {
     k_sleep(K_SECONDS(10));
     if (crypto_init() != 0) {
         printk("Couldn't init crypto");
+        goto ret;
     }
     struct net_buf_simple key_buf;
     psa_status_t err;
 
     printk("Beginning to flash device %d\n", CONFIG_FLASHED_DEVICE);
+    err = crypto_secure_counter_init(&counter);
+    if (err != PSA_SUCCESS) {
+        printk("Failed to init counter %d\n", err);
+        goto ret;
+    }
+
+    counter.value = 0;
+    err = crypto_secure_counter_commit(&counter);
+    if (err != PSA_SUCCESS) {
+        printk("Failed to commit counter\n");
+        goto ret;
+    }
+    printk("Successfully flashed counter\n");
+
 #if CONFIG_FLASHED_DEVICE == 0
     net_buf_simple_init_with_data(&key_buf, keys[0], KEY_LEN);
-    err = recreate_key(advertiser_key_id, key_buf);
+    err = recreate_key(ADVERTISER_KEY_ID, key_buf);
 
     if (err != PSA_SUCCESS)
         goto ret_fail;
@@ -42,7 +56,7 @@ int main() {
 
     for (size_t i = 1; i < ARRAY_SIZE(keys); i++) {
         net_buf_simple_init_with_data(&key_buf, keys[i], MAC_LEN);
-        err = recreate_key(min_scanner_key_id + i - 1, key_buf);
+        err = recreate_key(MIN_SCANNER_KEY_ID + i - 1, key_buf);
 
         if (err != PSA_SUCCESS)
             goto ret_fail;
@@ -50,13 +64,13 @@ int main() {
     }
 #else  // advertiser part
     net_buf_simple_init_with_data(&key_buf, keys[0], MAC_LEN);
-    err = recreate_key(advertiser_key_id, key_buf);
+    err = recreate_key(ADVERTISER_KEY_ID, key_buf);
     if (err != PSA_SUCCESS)
         goto ret_fail;
 
     net_buf_simple_init_with_data(
         &key_buf, keys[CONFIG_FLASHED_DEVICE], MAC_LEN);
-    err = recreate_key(min_scanner_key_id, key_buf);
+    err = recreate_key(MIN_SCANNER_KEY_ID, key_buf);
     if (err != PSA_SUCCESS)
         goto ret_fail;
 #endif // scanner part
@@ -67,5 +81,6 @@ int main() {
 
 ret_fail:
     printk("Failed to flash crypto key, (err: %d)", err);
+ret:
     k_sleep(K_FOREVER);
 }
